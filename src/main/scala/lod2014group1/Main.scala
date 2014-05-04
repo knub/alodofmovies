21c420d6723b30e7e7f64a673df37515bb1fd916
Stefan Bunk <stefan.bunk@googlemail.com>
2014-05-04 17:52:47 +0200

    Introduced command line parameters.

--- src/main/scala/lod2014group1/Main.scala
+++ src/main/scala/lod2014group1/Main.scala
@@ -4,20 +4,22 @@ import org.slf4s.Logging
 import lod2014group1.Config.Person
 import lod2014group1.triplification.Triplifier
 import java.io.File
+import scala.collection.JavaConversions._
 
 object Main extends App with Logging {
 
 	override def main(args: Array[String]): Unit = {
 		log.debug("Started.")
-		if (I.am == Person.Stefan) {
+		log.info("Arguments: " + args.toList)
+		if (args contains "triplify") {
 			val triplifier = new Triplifier
 			triplifier.triplify(new File("data/IMDBMovie/tt0109830/fullcredits.html"))
-		}	else if (I.am == Person.Tanja) {
-      val triplifier = new Triplifier
-      //triplifier.triplify(new File("data/IMDBMovie/tt0758758/locations.html"))
-      triplifier.triplify(new File("data/IMDBMovie/tt0054331/keywords.html"))
-    } else {
+			triplifier.triplify(new File("data/IMDBMovie/tt0054331/keywords.html"))
+			//triplifier.triplify(new File("data/IMDBMovie/tt0758758/locations.html"))
+		} else if (args contains "crawl") {
 			Crawler.crawl
+		} else {
+			log.warn("Please pass a parameter to indicate what you want to do, e.g. run `gradle crawl` or `gradle triplify`.")
 		}
 		log.debug("Finished.")
 	}
