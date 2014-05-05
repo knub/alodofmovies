268685873a0262bddd2b84049c96193e824edb4d
Ricarda Schueler <ricarda.schueler@gmx.de>
2014-05-05 09:25:52 +0200

    freebase api

--- src/main/scala/lod2014group1/Main.scala
+++ src/main/scala/lod2014group1/Main.scala
@@ -4,12 +4,16 @@ import org.joda.time.DateTime
 import lod2014group1.crawling.Crawler
 import java.io.File
 import org.slf4s.Logging
+import lod2014group1.apis.FreebaseAPI
 
 object Main extends App with Logging {
 
 	override def main(args: Array[String]): Unit = {
-		log.info("Started.")
-		Crawler.crawl;
-		log.info("Finished.")
+		//log.info("Started.")
+		//Crawler.crawl;
+		//log.info("Finished.")
+	  val freebaseApi = new FreebaseAPI()
+	  freebaseApi.getAllNotImdbMovies
+	  freebaseApi.getExampleRdf
 	}
 }
