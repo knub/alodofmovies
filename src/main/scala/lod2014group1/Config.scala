7fc405b6295806bdb179dceac0adc2978fb84fa7
Ricarda Schueler <ricarda.schueler@gmx.de>
2014-05-05 14:35:31 +0200

    freebase api key

--- src/main/scala/lod2014group1/Config.scala
+++ src/main/scala/lod2014group1/Config.scala
@@ -3,6 +3,7 @@ package lod2014group1
 object Config {
 	def DATA_FOLDER = "data"
 	def TMDB_API = "eac7eb8f612f92c83adf6a95ef34cdcc"
+	def FREEBASE_API_KEY = "./config/freebase.api-key"
 
 	object Person extends Enumeration {
 		type Person = Value
