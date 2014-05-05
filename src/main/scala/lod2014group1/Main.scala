e358a4c41caf880aa3ad6685e6579779a52b5cb8
Ricarda Schueler <ricarda.schueler@gmx.de>
2014-05-05 10:29:22 +0200

    cleaned some freebase stuff

--- src/main/scala/lod2014group1/Main.scala
+++ src/main/scala/lod2014group1/Main.scala
@@ -1,14 +1,11 @@
 package lod2014group1
 import lod2014group1.crawling.Crawler
 import org.slf4s.Logging
-<<<<<<< HEAD
 import lod2014group1.apis.FreebaseAPI
-=======
 import lod2014group1.Config.Person
 import lod2014group1.triplification.Triplifier
 import java.io.File
 import scala.collection.JavaConversions._
->>>>>>> 21c420d6723b30e7e7f64a673df37515bb1fd916
 
 object Main extends App with Logging {
 
@@ -23,6 +20,9 @@ object Main extends App with Logging {
 		} else if (args contains "crawl") {
 			Crawler.crawl
 		} else if (args contains "freebase") {
+		  val freebase = new FreebaseAPI()
+		  freebase.getAllNotImdbMovies
+		  //freebase.getExampleRdf
 		} else {
 			log.warn("Please pass a parameter to indicate what you want to do, e.g. run `gradle crawl` or `gradle triplify`.")
 		}
