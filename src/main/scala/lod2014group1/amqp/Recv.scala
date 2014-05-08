786b8e0dc3faee8765af932c313c376991b08d19
Dominik Mueller <isaak.dominik@gmail.com>
2014-05-08 20:57:29 +0200

    Added conf for tasks

--- src/main/scala/lod2014group1/amqp/Recv.scala
+++ src/main/scala/lod2014group1/amqp/Recv.scala
@@ -3,15 +3,21 @@ package lod2014group1.amqp
 import com.rabbitmq.client._
 import scala.pickling._
 import binary._
+import com.typesafe.config.ConfigFactory
 
 
 object Recv {
-
-	private val QUEUE_NAME = "hello"
+	val conf = ConfigFactory.load();
+	private val QUEUE_NAME = conf.getString("alodofmovies.hosts.localhost.queue")
+	private val HOST_NAME = conf.getString("alodofmovies.hosts.localhost.host")
 
 	def listen() {
 		val factory = new ConnectionFactory()
-		factory.setHost("localhost")
+		factory.setHost(HOST_NAME)
+		factory.setVirtualHost(conf.getString("alodofmovies.hosts.localhost.vhost"))
+		factory.setUsername(conf.getString("alodofmovies.hosts.localhost.username"))
+		factory.setPassword(conf.getString("alodofmovies.hosts.localhost.password"))
+
 		val connection = factory.newConnection()
 		val channel = connection.createChannel()
 		channel.queueDeclare(QUEUE_NAME, false, false, false, null)
--- src/main/scala/lod2014group1/amqp/Send.scala
+++ src/main/scala/lod2014group1/amqp/Send.scala
@@ -3,6 +3,7 @@ package lod2014group1.amqp
 import com.rabbitmq.client._
 import scala.pickling._
 import binary._
+import com.typesafe.config.ConfigFactory
 
 case class WorkerTask(msg: String, freq: Int)
 
@@ -10,13 +11,19 @@ case class WorkerTask(msg: String, freq: Int)
  * An Example of how to use the Example subclass of AMQPSender[T]. Still following?
  */
 object Send {
-	private val QUEUE_NAME = "hello"
+	val conf = ConfigFactory.load();
+	private val QUEUE_NAME = conf.getString("alodofmovies.hosts.localhost.queue")
+	private val HOST_NAME = conf.getString("alodofmovies.hosts.localhost.host")
 
 	def send(msg: String) {
 		val message = WorkerTask(msg, 4)
-		def printMessage(): Unit = println(" [x] Received '" + msg + "'")
+
 		val factory = new ConnectionFactory()
-		factory.setHost("localhost")
+		factory.setHost(HOST_NAME)
+		factory.setVirtualHost(conf.getString("alodofmovies.hosts.localhost.vhost"))
+		factory.setUsername(conf.getString("alodofmovies.hosts.localhost.username"))
+		factory.setPassword(conf.getString("alodofmovies.hosts.localhost.password"))
+
 		val connection = factory.newConnection()
 		val channel = connection.createChannel()
 		channel.queueDeclare(QUEUE_NAME, false, false, false, null)
