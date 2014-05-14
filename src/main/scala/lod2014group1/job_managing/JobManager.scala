787413e4807f7a2b6a7c29957e73fd5e7fba0aa9
Stefan Bunk <stefan.bunk@googlemail.com>
2014-05-14 17:56:27 +0200

    Basic console and database creation.

--- src/main/scala/lod2014group1/job_managing/JobManager.scala
+++ src/main/scala/lod2014group1/job_managing/JobManager.scala
@@ -2,6 +2,15 @@ package lod2014group1.job_managing
 
 import scala.slick.driver.SQLiteDriver.simple._
 import org.slf4s.Logging
+import scala.slick.jdbc.meta.MTable
+
+class Crawl(tag: Tag) extends Table[(Int, String, String)](tag, "crawls") {
+	def id = column[Int]("CRAWL_ID", O.PrimaryKey, O.AutoInc)
+	def uri = column[String]("URI")
+	def file = column[String]("FILE")
+
+	def * = (id, uri, file)
+}
 
 object JobManager extends App with Logging {
 
@@ -10,20 +19,23 @@ object JobManager extends App with Logging {
 	}
 
 	def run(): Unit = {
-		Database.forURL("jdbc:sqlite:test.db", driver = "org.sqlite.JDBC") withSession { implicit session =>
-			class Crawl(tag: Tag) extends Table[(Int, String, String)](tag, "CRAWL") {
-				def id = column[Int]("CRAWL_ID", O.PrimaryKey)
-				def uri = column[String]("URI")
-				def file = column[String]("FILE")
-
-				def * = (id, uri, file)
-			}
+		val database =  Database.forURL("jdbc:sqlite:test.db", driver = "org.sqlite.JDBC")
+		database withSession { implicit session =>
 			val crawls = TableQuery[Crawl]
+			if (MTable.getTables("crawls").list.isEmpty) {
+				crawls.ddl.create
+			} else {
+				println("Not creating table.")
+			}
 
+			crawls.insert(0, "abc", "def")
 
-			crawls.ddl.create
-
-			crawls += (1, "abc", "def")
+			var s = ""
+			while (s != "STOP") {
+				print("> ")
+				s = scala.Console.readLine
+				println(s)
+			}
 		}
 
 	}
