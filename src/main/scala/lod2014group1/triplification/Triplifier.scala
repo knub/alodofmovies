b7094e01b200f9e9a90e9974f6b19130368b8a75
Tanja Bergmann <tabergma@gmail.com>
2014-05-21 18:46:17 +0200

    Triplifying cast page almost completly.

--- src/main/scala/lod2014group1/triplification/Triplifier.scala
+++ src/main/scala/lod2014group1/triplification/Triplifier.scala
@@ -43,11 +43,11 @@ object Triplifier extends Logging {
 				triplifier.triplify(new File("data/IMDBMovie/tt0109830/fullcredits.html"))
 			}
 			case Config.Person.Tanja => {
-//				triplifier.triplify(new File("data/IMDBMovie/tt0758758/locations.html")) :::
-//				triplifier.triplify(new File("data/IMDBMovie/tt0054331/keywords.html")) :::
-//				triplifier.triplify(new File("data/IMDBMovie/tt0758758/awards.html")) :::
-//				triplifier.triplify(new File("data/IMDBMovie/tt0050900/releaseinfo.html")) :::
-//				triplifier.triplify(new File("data/IMDBMovie/tt0137523/tt0137523.html")) :::
+				triplifier.triplify(new File("data/IMDBMovie/tt0758758/locations.html")) :::
+				triplifier.triplify(new File("data/IMDBMovie/tt0054331/keywords.html")) :::
+				triplifier.triplify(new File("data/IMDBMovie/tt0758758/awards.html")) :::
+				triplifier.triplify(new File("data/IMDBMovie/tt0050900/releaseinfo.html")) :::
+				triplifier.triplify(new File("data/IMDBMovie/tt0137523/tt0137523.html")) :::
 				triplifier.triplify(new File("data/IMDBMovie/tt0109830/fullcredits.html"))
 			}
 			case Config.Person.Rice => {
