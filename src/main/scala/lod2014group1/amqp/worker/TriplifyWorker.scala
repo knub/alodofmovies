ad0e433721f5ac633f524557c6bb6d53652087c4
Stefan Bunk <stefan.bunk@googlemail.com>
2014-06-05 00:15:57 +0200

    Properly create crawl task.

--- src/main/scala/lod2014group1/amqp/worker/TriplifyWorker.scala
+++ src/main/scala/lod2014group1/amqp/worker/TriplifyWorker.scala
@@ -1,16 +1,23 @@
 package lod2014group1.amqp.worker
 
 import lod2014group1.rdf.{RdfResource, RdfTriple}
+import org.apache.commons.io.FileUtils
+import java.io.File
+import lod2014group1.triplification.Triplifier
 
 class TriplifyWorker extends Worker {
 
+	val triplifier = new Triplifier()
+
 	def execute(taskId: Long, params: Map[String, String]): TaskAnswer = {
-		val name = params("uri").replace("_", " ")
-		val s = RdfResource(name)
-		val triple = RdfTriple(s, s, s).toRdfTripleString()
+		val tmpFile = File.createTempFile("triplifyTmp", null, null)
+		tmpFile.deleteOnExit()
+		FileUtils.writeStringToFile(tmpFile, params("content"))
+		val triples = triplifier.triplify(tmpFile).map { _.toRdfTripleString() }
 		val answerMap: Map[String, String] = Map()
 
-		new TaskAnswer(taskId, answerMap, Nil, List(triple))
+		tmpFile.delete()
+		new TaskAnswer(taskId, answerMap, Nil, triples)
 	}
 
 }
--- src/main/scala/lod2014group1/amqp/worker/WorkerTask.scala
+++ src/main/scala/lod2014group1/amqp/worker/WorkerTask.scala
@@ -1,10 +1,20 @@
 package lod2014group1.amqp.worker
 
 import lod2014group1.database.Task
+import lod2014group1.amqp.TaskType
+import org.apache.commons.io.FileUtils
+import java.io.File
+import lod2014group1.Config
 
 object WorkerTask {
 	def fromDatabaseTask(dbTask: Task): WorkerTask = {
-		WorkerTask(dbTask.taskType, dbTask.id, Map("uri" -> dbTask.fileOrUrl))
+		if (TaskType.withName(dbTask.taskType) == TaskType.Triplify)
+			WorkerTask(dbTask.taskType, dbTask.id, Map("content" -> FileUtils.readFileToString(new File(s"${Config.DATA_FOLDER}/${dbTask.fileOrUrl}"))))
+		else if (TaskType.withName(dbTask.taskType) == TaskType.Crawl)
+			WorkerTask(dbTask.taskType, dbTask.id, Map("uri" -> dbTask.fileOrUrl))
+		else
+			throw new RuntimeException("Not handled yet.")
+
 	}
 }
 
