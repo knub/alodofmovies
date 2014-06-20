7430d5db0416ed59b4a210dbe81cbd07907700ef
Stefan Bunk <stefan.bunk@googlemail.com>
2014-06-20 23:11:24 +0200

    Added category to prefixes.

--- src/main/scala/lod2014group1/database/VirtuosoDatabase.scala
+++ src/main/scala/lod2014group1/database/VirtuosoDatabase.scala
@@ -137,6 +137,7 @@ class VirtuosoLocalDatabase(sparqlEndpoint: String) extends VirtuosoRemoteDataba
 			  |@prefix skos: <http://www.w3.org/2004/02/skos/core#> .
 			  |@prefix foaf: <http://xmlns.com/foaf/0.1/> .
 			  |@prefix lod: <http://purl.org/hpi/movie#> .
+			  |@prefix category: <http://dbpedia.org/resource/Category:> .
 			  |@prefix freebase: <http://rdf.freebase.com/ns/> .
 			""".stripMargin)
 	}
