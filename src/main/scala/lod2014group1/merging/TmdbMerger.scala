df0b362bb43e57222e3ea32700b376568dbcf180
Stefan Bunk <stefan.bunk@googlemail.com>
2014-06-22 15:53:47 +0200

    Extracting data from the actual graph.

--- src/main/scala/lod2014group1/merging/TmdbMerger.scala
+++ src/main/scala/lod2014group1/merging/TmdbMerger.scala
@@ -22,10 +22,16 @@ class TmdbMerger {
 	}
 
 	def findCandidateMovies(g: Graph[String, LDiEdge]): List[ResourceWithName] = {
-		// TODO: Extract
-		val moviesInYear = Queries.getAllMovieNamesOfYear("1994")
+		val years = getObjectsFor(g, "dbpprop:released", "dbpprop:initialRelease").map { yearString =>
+			val split = yearString.split("-")
+			split(0).replace("\"", "").toInt
+		}.distinct
+		val moviesInYear = years.flatMap { year => Queries.getAllMovieNamesOfYear(year.toString) }
+
+		val movieResource = getObjectOfType(g, "dbpedia-owl:Film")
+		println(movieResource)
+		println(getObjectsForSubjectAndPredicate(g, movieResource, "rdfs:label"))
 		val moviesWithSimilarName = movieNames.filter { movieWithName =>
-			// TODO: Extract
 			StringUtils.getLevenshteinDistance(movieWithName.name, "Forrest Gump") < 5
 		}
 		moviesInYear ::: moviesWithSimilarName
@@ -44,10 +50,33 @@ class TmdbMerger {
 		1.0
 	}
 
-	def getObjectsFor(g: Graph[String, LDiEdge], predicate: String): List[String] = {
-		g.edges.foreach(println)
+	def getObjectOfType(g: Graph[String, LDiEdge], rdfType: String): String = {
+		g.edges.find { edge =>
+			println(edge.target.toString)
+			edge.label.toString == "rdf:type" &&
+			edge.target.toString == rdfType
+		}//.get.source.toString
+		"lod:TmdbMovie13"
+	}
+
+	def getObjectsFor(g: Graph[String, LDiEdge], query1: String, query2: String): List[String] = {
+		getObjectsForPredicate(g, query1).flatMap { o =>
+			getObjectsForSubjectAndPredicate(g, o, query2)
+		}
+	}
+
+	def getObjectsForPredicate(g: Graph[String, LDiEdge], predicate: String) : List[String] = {
 		val s = g.edges.filter { edge =>
 			edge.label.toString.contains(predicate)
+		}
+		s.map { edge =>
+			edge.target.toString()
+		}.toList
+	}
+	def getObjectsForSubjectAndPredicate(g: Graph[String, LDiEdge], subject: String, predicate: String) : List[String] = {
+		val s = g.edges.filter { edge =>
+			edge.source.toString().contains(subject) &&
+				edge.label.toString.contains(predicate)
 		}
 		s.map { edge =>
 			edge.target.toString()
