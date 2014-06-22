084b8e24277f984ea49dc3c36adf9cc9672da03f
Stefan Bunk <stefan.bunk@googlemail.com>
2014-06-22 13:53:21 +0200

    Started on Merger.

--- src/main/scala/lod2014group1/merging/TmdbMerger.scala
+++ src/main/scala/lod2014group1/merging/TmdbMerger.scala
@@ -8,22 +8,31 @@ import scalax.collection.Graph
 class TmdbMerger {
 
 	val tmdbTriplifier = new TmdbMovieTriplifier
-	val triples = tmdbTriplifier.triplify(new File("data/TMDBMoviesList/movie/13.json"))
-	val edges = triples.map { triple =>
-		LDiEdge(triple.s, triple.o)(triple.p)
-	}
-	val g = Graph(edges: _*)
 
-	def mergeTmdb(): Unit = {
-		getObjectsFor("starring").foreach { o =>
-			getObjectsFor("dbprop:name").foreach {
+	def mergeForrestGump(): Unit = {
+		val triples = tmdbTriplifier.triplify(new File("data/TMDBMoviesList/movie/13.json"))
+		val edges = triples.map { triple =>
+			LDiEdge(triple.s.toString(), triple.o.toString)(triple.p.toString())
+		}
+		val g = Graph(edges: _*)
+		merge(g)
+	}
+
+	def findCandidateMovies(graph: Graph[String, LDiEdge]): List[String] = {
+		List()
+	}
+
+	def merge(triples: Graph[String, LDiEdge]): Unit = {
+		val candidates = findCandidateMovies(triples)
+		getObjectsFor(triples, "starring").foreach { o =>
+			getObjectsFor(triples, "dbprop:name").foreach {
 				println(_)
 			}
 		}
 
 	}
 
-	def getObjectsFor(predicate: String): List[String] = {
+	def getObjectsFor(g: Graph[String, LDiEdge], predicate: String): List[String] = {
 		val s = g.edges.filter { edge =>
 			edge.label.toString.contains(predicate)
 		}
