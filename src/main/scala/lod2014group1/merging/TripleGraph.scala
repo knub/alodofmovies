82d8d2dd0ec926c367a418d613a058d507490642
Stefan Bunk <stefan.bunk@googlemail.com>
2014-06-23 13:05:08 +0200

    Fixed the graph bug.

--- src/main/scala/lod2014group1/merging/TripleGraph.scala
+++ src/main/scala/lod2014group1/merging/TripleGraph.scala
@@ -2,7 +2,7 @@ package lod2014group1.merging
 
 import lod2014group1.rdf.{RdfResource, RdfTriple}
 import scalax.collection.edge.LDiEdge
-import scalax.collection.Graph
+import scalax.collection.mutable.Graph
 
 class TripleGraph(triples: List[RdfTriple]) {
 	val edges = triples.map { triple =>
@@ -15,10 +15,8 @@ class TripleGraph(triples: List[RdfTriple]) {
 		else s
 	}
 
-	val g = Graph(edges: _*)
-
 	def getObjectOfType(rdfType: String): String = {
-		g.edges.find { edge =>
+		edges.find { edge =>
 			edge.label.toString == "rdf:type" &&
 				edge.target.toString == rdfType
 		}//.get.source.toString
@@ -32,7 +30,7 @@ class TripleGraph(triples: List[RdfTriple]) {
 	}
 
 	def getObjectsForPredicate(predicate: String) : List[String] = {
-		val s = g.edges.filter { edge =>
+		val s = edges.filter { edge =>
 			edge.label.toString.contains(predicate)
 		}
 		s.map { edge =>
@@ -41,7 +39,7 @@ class TripleGraph(triples: List[RdfTriple]) {
 	}
 
 	def getObjectsForSubjectAndPredicate(subject: String, predicate: String) : List[String] = {
-		val s = g.edges.filter { edge =>
+		val s = edges.filter { edge =>
 			edge.source.toString() == subject &&
 				edge.label.toString.contains(predicate)
 		}
@@ -51,7 +49,7 @@ class TripleGraph(triples: List[RdfTriple]) {
 	}
 
 	def getTriplesForSubjectAndPredicate(subject: String, predicate: String) : List[RdfTriple] = {
-		val s = g.edges.filter { edge =>
+		val s = edges.filter { edge =>
 			edge.source.toString() == subject &&
 				edge.label.toString.contains(predicate)
 		}
@@ -62,7 +60,7 @@ class TripleGraph(triples: List[RdfTriple]) {
 	}
 
 	def getTriplesForSubject(subject: String) : List[RdfTriple] = {
-		val s = g.edges.filter { edge =>
+		val s = edges.filter { edge =>
 			edge.source.toString() == subject
 		}
 
